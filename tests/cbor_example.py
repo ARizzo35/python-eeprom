527f5d231c9548ceb4172a25f996ef4bc9e18dfd
Adam Rizkalla <adam.rizkalla@juul.com>
2020-11-09 17:29:16 -0800

    update tests

--- tests/cbor_example.py
+++ tests/cbor_example.py
@@ -15,6 +15,15 @@ verify_data = eeprom.read_file()
 print(verify_data)
 assert verify_data == test_data
 
+# Re-initialize to clear cached file
+del eeprom
+eeprom = CBOR_EEPROM("24c64", 0, 0x54)
+
+# Read data object
+val = eeprom.get('some_key')
+print(val)
+assert val == test_data['some_key']
+
 # Erase data file
 eeprom.erase_file()
 erased = eeprom.read_file()
